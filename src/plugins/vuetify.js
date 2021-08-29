36daaea390d05b852cf43a9b096c604ecffeb72a
wojtek <wojtekgrelewicz@gmail.com>
2021-08-29 19:02:02 +0200

    panel logowania oraz rejestracji

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
+Vue.use(Vuetify);
+Vue.component('menu-icon', MenuIcon);
+export default new Vuetify({
+
+    theme: {
+        themes: {
+            primary: colors.lightBlue.accent3,
+        }
+    }
+
+
+});
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+
+  devServer: {
+    port: 8084,
+    host: '0.0.0.0'
+  }
+}
